a84f0edd531d4e8c4028cac44cc3aaa6a7c30158
shailesh <sknewtons330@gmail.com>
2023-04-28 13:46:54 +0545

    blog section done

--- src/components/articles/Articles.jsx
+++ src/components/articles/Articles.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 import "./articles.css";
 
-function Articles() {
+
+function Articles({imageUrl , date, title}) {
   return (
-    <div>Articles</div>
+    <div className='gpt4__blog-container_article'>
+      <div className='gpt4__blog-container_article-image'>
+        <img src={imageUrl} alt="blog image" />
+      </div>
+      <div className='gpt4__blog-container_article-content'>
+        <div>
+          <p>{date}</p>
+          <h3>{title}</h3>
+        </div>
+        <p>Read Full Article</p>
+      </div>
+    </div>
   )
 }
 
--- src/containers/blogs/Blog.jsx
+++ src/containers/blogs/Blog.jsx
@@ -1,12 +1,27 @@
 import React from 'react';
 import "./blog.css";
+import Articles from '../../components/articles/Articles';
+import {blog01, blog02, blog03, blog04, blog05}  from "./blogimport";
 
 function Blog() {
   return (
-    <div className='gpt4__blog seaction__padding' id="blog">
-
+    <div className='gpt4__blog section__wrapper'>
+      <div className='gpt4__blog-heading'>
+        <h1 className='gradient__text'>A lot is happening, We are blogging about it.</h1>
+      </div>
+      <div className='gpt4__blog-container'>
+        <div className='gpt4__blog-container_groupA'>
+            <Articles imageUrl={blog01} date="jan 23, 2023" title="GPT-4 and Open  AI is the future. Let us exlore how it is?"/>
+        </div>
+        <div className='gpt4__blog-container_groupB'>
+          <Articles imageUrl={blog02} date="jan 23, 2023" title="GPT-4 and Open  AI is the future. Let us exlore how it is?"/>
+          <Articles imageUrl={blog03} date="jan 23, 2023" title="GPT-4 and Open  AI is the future. Let us exlore how it is?"/>
+          <Articles imageUrl={blog04} date="jan 23, 2023" title="GPT-4 and Open  AI is the future. Let us exlore how it is?"/>
+          <Articles imageUrl={blog04} date="jan 23, 2023" title="GPT-4 and Open  AI is the future. Let us exlore how it is?" />
+        </div>
+      </div>
     </div>
   )
 }
-
+ 
 export default Blog;
