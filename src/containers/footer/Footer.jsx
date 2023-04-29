7f8a34d7fddebc8045dc912a6278c087b8d56876
shailesh <shaileshkryadav13@gmail.com>
2023-04-29 08:13:59 +0545

    footer done and website is completed

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -1,9 +1,45 @@
 import React from 'react'
 import "./footer.css";
+import gpt4Logo from "../../assets/logo.svg";
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <div className='gpt4__footer section__padding'>
+      <div className='gpt4__footer-heading'>
+        <h1 className='gradient__text'>Do you want to step in to the future before others</h1>
+      </div>
+      <div className='gpt4__footer-btn'>
+        <p>Request Early Access</p>
+      </div>
+
+      <div className='gpt4__footer-links'>
+        <div className='gpt4__footer-links_logo'>
+          <img src={gpt4Logo} alt='logo' />
+          <p>Shailesh Yadav production, All Rights Reserved</p>
+        </div>
+        <div className='gpt4__footer-links_div'>
+          <h4>Links</h4>
+          <p>Overons</p>
+          <p>Social Media</p>
+          <p>Counters</p>
+          <p>Contact</p>
+        </div>
+        <div className='gpt4__footer-links_div'>
+          <h4>Company</h4>
+          <p>Terms & Conditions</p>
+          <p>Privacy Policy</p>
+          <p>Contact</p>
+        </div>
+        <div className='gpt4__footer-links_div'>
+          <h4>Get in touch</h4>
+          <p>Kathmandu, Nepal</p>
+          <p>shaileshkryadav13@gmail.com</p>
+        </div>
+      </div>
+      <div className='gpt3__footer-copyright'>
+        <p>© 2023 GPT-4. All rights reserved.</p>
+      </div>
+    </div>
   )
 }
 
